d14933a7e7dd25cb8f5f113cc15314c18e8c440d
Gloryofthe80s <gloryoftheeighties@gmail.com>
2014-02-13 03:09:41 -0500

    Printed tasks are now editable! Also - a major overhaul of events with delegation properly in place.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -23,98 +23,181 @@ var taskArray = [];
 
 $(document).ready(function() {
 
-// remove placeholder text on focus
-$('.create-new-task').on('focus', function() {
-    $(this).removeAttr('placeholder');
-    $(this).css('outline', 'none');
-    $(this).css('text-align', 'left');
-});
+    // style task input on focus
+    $('.create-new-task').on('focus', function() {
+        $(this).removeAttr('placeholder');
+        $(this).css('outline', 'none');
+        $(this).css('text-align', 'left');
+    });
 
-// re-add placeholder text on focus out
-$('.create-new-task').on('focusout', function() {
-    $(this).attr('placeholder', 'I need to...');
-    $(this).css('text-align', 'center');
-});
+    // re-add placeholder text on focus out
+    $('.create-new-task').on('focusout', function() {
+        $(this).attr('placeholder', 'I need to...');
+        $(this).css('text-align', 'center');
+    });
 
-// submit task creation on enter keypress (and hover effects)
-$('.create-new-task').keypress(function(e) {
-    //on enter keypress, so long as the input isn't empty
-    if(e.which == 13 && $(this).val() != '') {
-        var input = {
-            task: $('.create-new-task').val(),
-        };
+    // submit task creation on enter keypress (and hover effects)
+    $('.create-new-task').on('keypress', function(e) {
+        //on enter keypress, so long as the input isn't empty
+        if(e.which == 13 && $(this).val() != '') {
+            var input = {
+                task: $('.create-new-task').val(),
+            };
 
-        // construct the task object and put it in the taskArray
-        taskArray.push(new TaskObject(input));
+            // construct the task object and put it in the taskArray
+            taskArray.push(new TaskObject(input));
 
-        //'wipe the slate' of tasks
-        $('.printed-task-container').html('');
+            //'wipe the slate' of tasks
+            $('.printed-task-container').html('');
 
-        //reprint all tasks in the taskArray
-        _.each(taskArray, function(taskObjLit, i) {
-            $('.printed-task-container').append(taskTemplate(taskObjLit));
+            //reprint all tasks in the taskArray
+            _.each(taskArray, function(taskObjLit, i) {
+                $('.printed-task-container').append(taskTemplate(taskObjLit));
+            });
+
+            //clear the text input
+            $(this).val('');
+
+    }});
+    //fade in buttons on mouseenter
+    $('.printed-task-container').on('mouseenter', '.task-wrapper', function(event) {
+        event.preventDefault();
+        $(this).find('.task-btns').stop();
+        $(this).find('.task-btns').animate({opacity: 1}, 150);
+    });
+
+    //fade out buttons on mouseleave
+    $('.printed-task-container').on('mouseleave', '.task-wrapper', function(event) {
+        event.preventDefault();
+        $(this).find('.task-btns').stop();
+        $(this).find('.task-btns').animate({opacity: 0}, 150);
+    });
+
+    //turn trash button red on mouseenter
+    $('.printed-task-container').on('mouseenter', '.btn-trash-task', function(event) {
+        event.preventDefault();
+        $(this).addClass('hovered');
+    });
+
+    //remove red on mouseleave
+    $('.printed-task-container').on('mouseleave', '.btn-trash-task', function(event) {
+        event.preventDefault();
+        $(this).removeClass('hovered');
+    });
+
+    // on 'completed' button click
+    $('.printed-task-container').on('click', '.btn-mark-completed', function() {
+        $(this).closest('.task-wrapper').toggleClass('completed');
+
+        //find the corresponding task object in the taskArray
+        var taskToBeFlaggedCompleted = _.findWhere(taskArray, {uniqueId : $(this).closest('.task-wrapper').attr('id')});
+
+        // toggle completed prop of the task object
+        _.each(taskArray, function(task, index) {
+            if(task.uniqueId == taskToBeFlaggedCompleted.uniqueId) {
+                if( $(taskToBeFlaggedCompleted).prop('complete') == false) {
+                    $(taskToBeFlaggedCompleted).prop('complete', true);
+                } else {
+                    $(taskToBeFlaggedCompleted).prop('complete', false);
+                }
+            };
+        });
+    });
+
+    // editing printed tasks: on double click
+    $('.printed-task-container').on('dblclick', '.task-side-left', function () {
+        var clickedBoxId = $(this).parent('.task-wrapper').attr('id');
+        console.log(clickedBoxId); //working
+
+        //inject the inputbox, store pre-edited task
+        var currentValue = $(this).find('.task-p').text();
+        var inputbox = "<input type='text' class='task-input-box'>";
+        $(this).find($('.task-name')).html(inputbox);
+        var theEditInput = $(this).find('.task-input-box');
+        theEditInput.prop('value', currentValue);
+        theEditInput.focus();
+
+        //reprint once focus is lost
+        theEditInput.blur(function() {
+            var newValue = $('.task-input-box').val();
+            console.log(newValue); //working
+
+            //find the corresponding task in taskArray and update its value
+            var taskToBeUpdated = _.findWhere(taskArray, {uniqueId : clickedBoxId});
+
+            _.each(taskArray, function(el, i) {
+                if (el.uniqueId === taskToBeUpdated.uniqueId) {
+                    $(taskToBeUpdated).prop('task', newValue);
+                }
+            });
+
+            //wipe tasks and repreint
+            $('.printed-task-container').html('');
+
+            _.each(taskArray, function(taskObjLit, i) {
+                $('.printed-task-container').append(taskTemplate(taskObjLit));
+            });
+        });
+    })
+
+    // on 'trash' button click
+    $('.printed-task-container').on('click', '.btn-trash-task', function() {
+        $(this).closest('.task-wrapper').fadeOut('fast', function() {
+            $(this).closest('.task-wrapper').remove();
         });
 
-        //clear the text input
-        $(this).val('');
-    };
+        var taskToBeDeleted = _.findWhere(taskArray, {uniqueId : $(this).closest('.task-wrapper').attr('id')});
 
-    // on printed task hover
-    $('.task-wrapper').on({
-        mouseenter: function () {
-            $(this).find('.task-btns').stop();
-            $(this).find('.task-btns').animate({opacity: 1}, 150);
-        },
-        mouseleave: function () {
-            $(this).find('.task-btns').stop();
-            $(this).find('.task-btns').animate({opacity: 0}, 150);
-        }
+        _.each(taskArray, function(task, index) {
+            if (task.uniqueId == taskToBeDeleted.uniqueId) {
+                taskArray.splice(index, 1);
+            };
+        });
     });
 
-    // on trash icon hover
-    $('.btn-trash-task').on({
-        mouseenter: function () {
-            $(this).addClass('hovered');
-        },
-        mouseleave: function () {
-            $(this).removeClass('hovered');
-        }
-    });
-});
+    // ------ TESTING STUFF ------
 
-// on 'completed' button click
-$('.printed-task-container').on('click', '.btn-mark-completed', function() {
-    $(this).closest('.task-wrapper').toggleClass('completed');
+    // taskArray = [
+    //         {
+    //             task : 'do laundry',
+    //             complete : false,
+    //             uniqueId : _.uniqueId('task_')
+    //         },
+    //         {
+    //             task : 'get some milk',
+    //             complete : false,
+    //             uniqueId : _.uniqueId('task_')
+    //         },
+    //         {
+    //             task : 'wash your filthy self',
+    //             complete : false,
+    //             uniqueId : _.uniqueId('task_')
+    //         },
+    //         {
+    //             task : 'crush your foes',
+    //             complete : false,
+    //             uniqueId : _.uniqueId('task_')
+    //         },
+    //     ];
 
-    //find the corresponding task object in the taskArray
-    var taskToBeFlaggedCompleted = _.findWhere(taskArray, {uniqueId : $(this).closest('.task-wrapper').attr('id')});
+    //     // before each it() function, do this
+    //     beforeEach(function(done){
+    //         console.log('clearing out the tasks div!');
 
-    // toggle completed prop of the task object
-    _.each(taskArray, function(task, index) {
-        if(task.uniqueId == taskToBeFlaggedCompleted.uniqueId) {
-            if( $(taskToBeFlaggedCompleted).prop('complete') == false) {
-                $(taskToBeFlaggedCompleted).prop('complete', true);
-            } else {
-                $(taskToBeFlaggedCompleted).prop('complete', false);
-            }
-        };
-    });
-});
+    //         // clear out the tasks div
+    //         $('.printed-task-container').html('');
 
-// on 'trash' button click
-$('.printed-task-container').on('click', '.btn-trash-task', function() {
-    $(this).closest('.task-wrapper').fadeOut('fast', function() {
-        $(this).closest('.task-wrapper').remove();
-    });
+    //         // loop over the fake taskArray
+    //         _.each(taskArray, function (taskObjLit, i) {
+    //             // inject it into the DOM
+    //             $('.printed-task-container').append(taskTemplate(taskObjLit));
 
-    var taskToBeDeleted = _.findWhere(taskArray, {uniqueId : $(this).closest('.task-wrapper').attr('id')});
+    //             // make sure we call done() after the last one is injected
+    //             // so that the actual test can then run
+    //             if (taskArray.length === i + 1) { done(); }
+    //         })
+    //     });
 
-    _.each(taskArray, function(task, index) {
-        if (task.uniqueId == taskToBeDeleted.uniqueId) {
-            taskArray.splice(index, 1);
-        };
-    });
-});
 
 }); //---- end JS wrapper
 
--- test/spec/test.js
+++ test/spec/test.js
@@ -3,47 +3,6 @@
 (function () {
     'use strict';
 
-    taskArray = [
-        {
-            task : 'do laundry',
-            complete : false,
-            uniqueId : _.uniqueId('task_')
-        },
-        {
-            task : 'get some milk',
-            complete : false,
-            uniqueId : _.uniqueId('task_')
-        },
-        {
-            task : 'wash your filthy self',
-            complete : false,
-            uniqueId : _.uniqueId('task_')
-        },
-        {
-            task : 'crush your foes',
-            complete : false,
-            uniqueId : _.uniqueId('task_')
-        },
-    ];
-
-    // before each it() function, do this
-    beforeEach(function(done){
-        console.log('clearing out the tasks div!');
-
-        // clear out the tasks div
-        $('.printed-task-container').html('');
-
-        // loop over the fake tasksArray
-        _.each(taskArray, function (taskObjLit, i) {
-            // inject it into the DOM
-            $('.printed-task-container').append(taskTemplate(taskObjLit));
-
-            // make sure we call done() after the last one is injected
-            // so that the actual test can then run
-            if (taskArray.length === i + 1) { done(); }
-        })
-    });
-
     describe('printed tasks have', function () {
 
             describe('a complete button', function () {
@@ -51,15 +10,9 @@
                 it('on click, adds the completed class to the corresponding task div in the DOM, and sets task object prop to complete: true', function () {
 
                     var taskStatus = [];
-
                     $('#task_1').find('.btn-mark-completed').click();
-                    console.log('taskArray',taskArray);
-                    _.each(taskArray, function(element, index) {
-                        taskStatus.push(element.complete);
-                        console.log('taskStatus',taskStatus)
-                    })
 
-                    // expect($('#task_1').hasClass('completed')).to.equal(true);
+                    expect($('#task_1').hasClass('completed')).to.equal(true);
 
                     // //check that only only task_1 (at index 0) is set to complete: true
                     expect(taskStatus[0]).to.be.equal(true);
@@ -69,7 +22,6 @@
                 });
 
                 it('if clicked again, toggles the task back to an uncompleted state (removes class and sets task object prop to complete: false)', function () {
-
 
                     $('#task_1').find('.btn-mark-completed').click();
                     $('#task_1').find('.btn-mark-completed').click();
@@ -94,7 +46,7 @@
             it('on click, should remove the corresponding task element from the DOM', function () {
                 $('#task_1').find('.btn-trash-task').click();
 
-                expect($('#task_1').hasClass('completed'));
+                expect($('#task_1').hasClass('completed')).to.be.equal();
             });
 
             it('on click, should remove the corresponding task object from the taskArray', function () {
